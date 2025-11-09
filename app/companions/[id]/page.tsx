e1d2022c6eea0f4c90bd08d59edf895a9b87c8dc
Dickson <dcksnteo@gmail.com>
2025-11-09 21:03:33 +0700

    implement search & filter, session detail companion

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,5 +1,53 @@
-const CompanionSession = () => {
-  return <div>CompanionSession</div>;
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+interface CompanionSessionPageProps {
+  params: Promise<{ id: string }>;
+}
+
+// params /url/{id} => id
+// searchParams /url?key=value&key2=value2
+
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params;
+  const { name, subject, topic, title, duration } = await getCompanion(id);
+  const user = await currentUser();
+
+  if (!user) redirect("/sign-in");
+  if (!name) redirect("/companion");
+
+  return (
+    <main>
+      <article className="rounded-border flex justify-between p-6 max-md:flex-col">
+        <div className="flex items-center gap-2">
+          <div
+            className="flex size-[72px] items-center justify-center rounded-lg max-md:hidden"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <Image
+              src={`/icons/${subject}.svg`}
+              alt={subject}
+              width={35}
+              height={35}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <p className="text-2xl font-bold">{name}</p>
+              <div className="subject-badge max-sm:hidden">{subject}</div>
+            </div>
+            <p className="text-lg">{topic}</p>
+          </div>
+        </div>
+        <div className="items-start text-2xl max-md:hidden">
+          {duration} minutes
+        </div>
+      </article>
+    </main>
+  );
 };
 
 export default CompanionSession;
